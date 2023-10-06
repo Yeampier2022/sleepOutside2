9e22bc5a167d09bd3237a13a65587fb28dbe8336
Yeampier2022 <yeampierjesusa@gmail.com>
2023-10-06 15:33:56 -0400

    feat: back commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,5 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import ProductList from "./ProductList.mjs";
 
 loadHeaderFooter();
 
-const dataSource = new ProductList("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductList("Tents", dataSource, element);
-
-listing.init();
 
